31b0f18ad81b6d8ec31427bd1bf7d41c86f87195
antoniosigrist <antoniosigrist@charlo.com.br>
2016-11-25 23:34:56 -0200

    Adicionando o arquivo socket.io.js para criar nicknames para os usuarios

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var i = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+	
+	var numeroUsuarios = 0;
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+
+	var usuarioAdicionado = false;
+
+  console.log("a new member"+i+" came in");
+  i++;
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', "MEMBRO" + i + ": "+ msg );
+
   });
 });
 
